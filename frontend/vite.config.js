9a69cb568c7f87b4eb8efbdf60bcbc9f08ff5d38
sam29sahil <sahil2004samyal@gmail.com>
2026-08-28 23:27:01 +0530

    vite:

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,7 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig, loadEnv } from "vite";
+﻿import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-
-  console.log("BUILD MODE:", mode);
-  console.log("API URL:", env.VITE_API_BASE_URL);
-
-  return {
-    plugins: [react()],
-  };
+export default defineConfig({
+  plugins: [react()],
+  envDir: ".",
 });
